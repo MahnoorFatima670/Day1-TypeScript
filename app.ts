7bce83aad40ffbfa9970c78040c16eb0cd5a1b24
Mahnoor Fatima <mhafiza038@gmail.com>
2024-08-08 21:04:26 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,32 @@
+
+//problem no:1
+//how sum all the numbers that are in an array?
+function sumArray(numbers:number[]):number
+{
+ const myarray=numbers;
+ return myarray.reduce((acc,curr)=>acc+curr,0);
+}
+console.log(sumArray([10,20,30,40,50]));
+console.log(sumArray([10,5,70,98,30]));
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([19,45,87,35,65]));
+
+//problem no:2
+//how reverse any string?
+var myString="hello";
+var ArrayHasMade=myString.split('');
+var reverseArray=ArrayHasMade.reverse();
+var reverseString=reverseArray.join('');
+console.log(reverseString);
+
+ myString="world";
+ArrayHasMade=myString.split('');
+reverseArray=ArrayHasMade.reverse();
+reverseString=reverseArray.join('');
+console.log(reverseString);
+
+myString="i am mahnoor";
+ArrayHasMade=myString.split('');
+reverseArray=ArrayHasMade.reverse();
+reverseString=reverseArray.join('');
+console.log(reverseString);
